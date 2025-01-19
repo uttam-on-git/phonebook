2dfc75a5eaf66a3f19755998b38d8790a1ce12b5
Romendra Uttam <120070407+uttam-on-git@users.noreply.github.com>
2025-01-20 03:07:14 +0530

    add static file serving and update API base URL for phonebook client

--- phonebook_client/src/services/persons.js
+++ phonebook_client/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl  = 'https://phonebook-vv35.onrender.com/api/persons'
+const baseUrl  = '/api/persons'
 
 const getAllPersons = () => {
     const request = axios.get(baseUrl);
--- phonebook_server/index.js
+++ phonebook_server/index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('dist'))
 
 let persons = [
   {
